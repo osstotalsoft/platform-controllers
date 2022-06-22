974855cf746e54b20515d8a62096357af229d8e6
Honeypot <lghinet@totalsoft.ro>
2022-06-22 14:19:44 +0300

    add logging

--- internal/controllers/provisioning_controller.go
+++ internal/controllers/provisioning_controller.go
@@ -255,7 +255,7 @@ func (c *ProvisioningController) syncHandler(key string) error {
 			if err != nil {
 				result.Error = c.migrator(p.Spec.TargetNamespace, tenant)
 			} else {
-				utilruntime.HandleError(err)
+				klog.ErrorS(err, "platform not found", "msg", err.Error())
 			}
 		}
 	}
