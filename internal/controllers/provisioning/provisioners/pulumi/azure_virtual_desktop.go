a7369381655418dea1424a9415358bb976d35b38
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2023-06-12 18:16:13 +0300

    AVD Fixed application

--- internal/controllers/provisioning/provisioners/pulumi/azure_virtual_desktop.go
+++ internal/controllers/provisioning/provisioners/pulumi/azure_virtual_desktop.go
@@ -544,15 +544,15 @@ func azureVirtualDesktopDeployFunc(platform string, tenant *platformv1.Tenant, r
 
 			for _, app := range azureVM.Spec.Applications {
 				_, err = desktopvirtualization.NewApplication(ctx, fmt.Sprintf("%s-apps-%s", hostPoolName, app.Name), &desktopvirtualization.ApplicationArgs{
-					ApplicationGroupName: pulumi.String(fmt.Sprintf("%s-apps", hostPoolName)),
-					ApplicationName:      pulumi.String(app.Name),
-					FriendlyName:         pulumi.String(app.FriendlyName),
-					FilePath:             pulumi.String(app.Path),
-					IconPath:             pulumi.String(app.Path),
-					IconIndex:            pulumi.Int(0),
-					CommandLineSetting:   pulumi.String(desktopvirtualization.CommandLineSettingDoNotAllow),
-					ShowInPortal:         pulumi.Bool(true),
-					ResourceGroupName:    resourceGroupName,
+					ApplicationGroupName: avd.RemoteAppGroup.Name,
+					//ApplicationName:      pulumi.String(app.Name),
+					FriendlyName:       pulumi.String(app.FriendlyName),
+					FilePath:           pulumi.String(app.Path),
+					IconPath:           pulumi.String(app.Path),
+					IconIndex:          pulumi.Int(0),
+					CommandLineSetting: pulumi.String(desktopvirtualization.CommandLineSettingDoNotAllow),
+					ShowInPortal:       pulumi.Bool(true),
+					ResourceGroupName:  resourceGroupName,
 				}, pulumi.Parent(avd.RemoteAppGroup))
 
 				if err != nil {
@@ -611,7 +611,7 @@ func azureVirtualDesktopDeployFunc(platform string, tenant *platformv1.Tenant, r
 			vms := make([]*AzureVirtualDesktopVM, azureVM.Spec.VmNumberOfInstances)
 
 			for i := 0; i < azureVM.Spec.VmNumberOfInstances; i++ {
-				vmName := fmt.Sprintf("%s-%d-", azureVM.Spec.HostPoolName, i)
+				vmName := fmt.Sprintf("%s-%d", azureVM.Spec.HostPoolName, i)
 				avdVM, err := NewAzureVirtualDesktopVM(ctx, vmName, &AzureVirtualDesktopVMArgs{
 					ResourceGroupName: resourceGroupName,
 					TenantName:        pulumi.String(tenant.Name),
