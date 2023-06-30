d7fb6d1eecf8d1c804fc2247ff62c40b9c2c5afe
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2023-06-30 16:48:21 +0300

    Deleted unused file

--- pkg/apis/provisioning/v1alpha1/tenantOverrideTypes.go
+++
@@ -1,7 +0,0 @@
-package v1alpha1
-
-import apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-
-type HasTenantOverrides1 interface {
-	GetTenantOverrides() map[string]*apiextensionsv1.JSON
-}
