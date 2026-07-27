4633995db0f870e3f4e00f5af1dd2f10733718bb
Lucian Ghinet <lghinet@totalsoft.ro>
2026-07-27 11:12:21 +0300

    feat: add service account realm roles to Keycloak client configuration

--- internal/controllers/provisioning/provisioners/pulumi/keycloak_client.go
+++ internal/controllers/provisioning/provisioners/pulumi/keycloak_client.go
@@ -98,6 +98,28 @@ func deployKeycloakClient(target provisioning.ProvisioningTarget,
 		return nil, err
 	}
 
+	// The service account user only exists when service accounts are enabled on the client.
+	// Looking it up otherwise would fail the Keycloak invoke at runtime.
+	if spec.ServiceAccountsEnabled && len(spec.ServiceAccountRealmRoles) > 0 {
+		// Get the service account user associated with this client
+		saUser := openid.GetClientServiceAccountUserOutput(ctx, openid.GetClientServiceAccountUserOutputArgs{
+			RealmId:  pulumi.String(realm.Id),
+			ClientId: client.ID().ToStringOutput(),
+		}, nil)
+
+		for _, role := range spec.ServiceAccountRealmRoles {
+			_, err = openid.NewClientServiceAccountRealmRole(ctx, fmt.Sprintf("%s-%s-service-account-role", keycloakClient.Name, role),
+				&openid.ClientServiceAccountRealmRoleArgs{
+					RealmId:              pulumi.String(realm.Id),
+					ServiceAccountUserId: saUser.Id(),
+					Role:                 pulumi.String(role),
+				}, pulumi.Parent(client))
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
 	// Deploy protocol mappers
 	for _, pm := range spec.ProtocolMappers {
 		config := pulumi.StringMap{}
@@ -155,54 +177,8 @@ func deployKeycloakClient(target provisioning.ProvisioningTarget,
 		}
 	}
 
-	if spec.Organization != "" {
-		// Get the service account user associated with this client
-		saUser := openid.GetClientServiceAccountUserOutput(ctx, openid.GetClientServiceAccountUserOutputArgs{
-			RealmId:  pulumi.String(realm.Id),
-			ClientId: client.ID().ToStringOutput(),
-		}, nil)
-
-		// Use Keycloak REST API to add service account to organization
-		// POST /admin/realms/{realm}/organizations/{orgId}/members
-		keycloakURL := os.Getenv("KEYCLOAK_URL")
-		keycloakClientID := os.Getenv("KEYCLOAK_CLIENT_ID")
-		keycloakClientSecret := os.Getenv("KEYCLOAK_CLIENT_SECRET")
-
-		if keycloakURL == "" || keycloakClientID == "" || keycloakClientSecret == "" {
-			return nil, fmt.Errorf("KEYCLOAK_URL, KEYCLOAK_CLIENT_ID and KEYCLOAK_CLIENT_SECRET environment variables must be set")
-		}
-		tc := provisioning.GetTemplateContext(target)
-		orgId, err := template.ParseTemplate(spec.Organization, tc)
-
-		createCmd := saUser.Id().ApplyT(func(userId string) string {
-			return fmt.Sprintf(
-				`TOKEN=$(curl -s -X POST "$KEYCLOAK_URL/realms/master/protocol/openid-connect/token" `+
-					`-d "grant_type=client_credentials&client_id=$KEYCLOAK_CLIENT_ID&client_secret=$KEYCLOAK_CLIENT_SECRET" | jq -r .access_token) && `+
-					`ORG_ID=$(curl -s -X GET "$KEYCLOAK_URL/admin/realms/%s/organizations?q=tid:%s" `+
-					`-H "Authorization: Bearer $TOKEN" | jq -r '.[0].id') && `+
-					`curl --fail-with-body -s -X POST "$KEYCLOAK_URL/admin/realms/%s/organizations/$ORG_ID/members" `+
-					`-H "Authorization: Bearer $TOKEN" `+
-					`-H "Content-Type: application/json" -d '"%s"'`,
-				spec.Realm, orgId, spec.Realm, userId,
-			)
-		}).(pulumi.StringOutput)
-
-		//interpreter := []string{"/bin/sh", "-c"}
-		//if runtime.GOOS == "windows" {
-		//	interpreter = []string{"C:\\Program Files\\Git\\bin\\bash.exe", "-c"}
-		//}
-
-		_, err = local.NewCommand(ctx,
-			fmt.Sprintf("%s-add-member-organization", keycloakClient.Name),
-			&local.CommandArgs{
-				Create: createCmd,
-				//Interpreter: pulumi.ToStringArray(interpreter),
-				Environment: pulumi.StringMap{
-					"KEYCLOAK_URL":           pulumi.String(keycloakURL),
-					"KEYCLOAK_CLIENT_ID":     pulumi.String(keycloakClientID),
-					"KEYCLOAK_CLIENT_SECRET": pulumi.String(keycloakClientSecret),
-				},
-			}, pulumi.Parent(client))
+	if spec.Organization != "" && spec.ServiceAccountsEnabled {
+		err = addToOrganization(target, client, ctx, keycloakClient.Name, realm.Id, spec.Realm, spec.Organization)
 		if err != nil {
 			return nil, err
 		}
@@ -226,4 +202,61 @@ func deployKeycloakClient(target provisioning.ProvisioningTarget,
 	}
 
 	return client, nil
+}
+
+func addToOrganization(target provisioning.ProvisioningTarget,
+	client *openid.Client, ctx *pulumi.Context, keycloakClientName string,
+	realmId string, realm string, organization string) error {
+
+	// Get the service account user associated with this client
+	saUser := openid.GetClientServiceAccountUserOutput(ctx, openid.GetClientServiceAccountUserOutputArgs{
+		RealmId:  pulumi.String(realmId),
+		ClientId: client.ID().ToStringOutput(),
+	}, nil)
+
+	// Use Keycloak REST API to add service account to organization
+	// POST /admin/realms/{realm}/organizations/{orgId}/members
+	keycloakURL := os.Getenv("KEYCLOAK_URL")
+	keycloakClientID := os.Getenv("KEYCLOAK_CLIENT_ID")
+	keycloakClientSecret := os.Getenv("KEYCLOAK_CLIENT_SECRET")
+
+	if keycloakURL == "" || keycloakClientID == "" || keycloakClientSecret == "" {
+		return fmt.Errorf("KEYCLOAK_URL, KEYCLOAK_CLIENT_ID and KEYCLOAK_CLIENT_SECRET environment variables must be set")
+	}
+	tc := provisioning.GetTemplateContext(target)
+	orgId, err := template.ParseTemplate(organization, tc)
+	if err != nil {
+		return err
+	}
+
+	createCmd := saUser.Id().ApplyT(func(userId string) string {
+		return fmt.Sprintf(
+			`TOKEN=$(curl -s -X POST "$KEYCLOAK_URL/realms/master/protocol/openid-connect/token" `+
+				`-d "grant_type=client_credentials&client_id=$KEYCLOAK_CLIENT_ID&client_secret=$KEYCLOAK_CLIENT_SECRET" | jq -r .access_token) && `+
+				`ORG_ID=$(curl -s -X GET "$KEYCLOAK_URL/admin/realms/%s/organizations?q=tid:%s" `+
+				`-H "Authorization: Bearer $TOKEN" | jq -r '.[0].id') && `+
+				`curl --fail-with-body -s -X POST "$KEYCLOAK_URL/admin/realms/%s/organizations/$ORG_ID/members" `+
+				`-H "Authorization: Bearer $TOKEN" `+
+				`-H "Content-Type: application/json" -d '"%s"'`,
+			realm, orgId, realm, userId,
+		)
+	}).(pulumi.StringOutput)
+
+	//interpreter := []string{"/bin/sh", "-c"}
+	//if runtime.GOOS == "windows" {
+	//	interpreter = []string{"C:\\Program Files\\Git\\bin\\bash.exe", "-c"}
+	//}
+
+	_, err = local.NewCommand(ctx,
+		fmt.Sprintf("%s-add-member-organization", keycloakClientName),
+		&local.CommandArgs{
+			Create: createCmd,
+			//Interpreter: pulumi.ToStringArray(interpreter),
+			Environment: pulumi.StringMap{
+				"KEYCLOAK_URL":           pulumi.String(keycloakURL),
+				"KEYCLOAK_CLIENT_ID":     pulumi.String(keycloakClientID),
+				"KEYCLOAK_CLIENT_SECRET": pulumi.String(keycloakClientSecret),
+			},
+		}, pulumi.Parent(client))
+	return err
 }
